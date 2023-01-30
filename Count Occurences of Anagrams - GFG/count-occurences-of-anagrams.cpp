515ff3c2c522fad8ba34add5f9a09bf319722b2c
iamvibhavtripathi <79766183+iamvibhavtripathi@users.noreply.github.com>
2023-01-30 15:02:51 +0530

    Added solution - LeetHub

--- Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
+++ Count Occurences of Anagrams - GFG/count-occurences-of-anagrams.cpp
@@ -8,86 +8,48 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	int search(string p, string s) {
+	int search(string pat, string txt) {
 	    // code here
-	   // int i=0,j=0;
-	   // int n= txt.size();
-	   // unordered_map<char,int> mp;
-	   // int k= pat.size();
-	   // for(int m=0; m<k; m++){
-	   //     mp[pat[m]]++;
-	   // }
-	   // int ans=0;
-	   // int count= mp.size();
-	   // while(j<n){
-	   //     //calculations
-	   //     if(mp.find(txt[j])!=mp.end()){
-	   //         mp[txt[j]]--;
-	   //     }
-	   //     if(mp[txt[j]]==0){
-	   //         count--;
-	   //     }
-    //         //creating window
-    //         if(j-i+1 < k){
-    //             j++;
-    //         }
-	   //     //size check 
-	   //     else if(j-i+1 == k){
-	   //         if(count == 0){
-	   //             ans++;
-	   //         }
-	   //         if(mp.find(txt[i])!=mp.end()){
-	   //                 mp[txt[i]]++;
-	   //                  if(mp[txt[i]]==1){
-
-	   //                 count++;
-	   //                  }
-	   //             }
-	   //             i++;j++;
-	            
-	   //     }
+	    int i=0,j=0;
+	    int n= txt.size();
+	    unordered_map<char,int> mp;
+	    int k= pat.size();
+	    for(int m=0; m<k; m++){
+	        mp[pat[m]]++;
+	    }
+	    int ans=0;
+	    int count= mp.size();
+	    while(j<n){
+	        //calculations
+	        if(mp.find(txt[j])!=mp.end()){
+	            mp[txt[j]]--;
 	        
-	   // }
-	   // return ans;
-	           unordered_map<char,int>mp;
-        vector<int>ans;
-        int k= p.size(), i=0, j=0, n=s.size();
-        //storing elements in a map
-        for(int it=0; it<k ; it++){
-            mp[p[it]]++;
-        }
-        int count=mp.size();
-        //sliding window
-        while(j<n){
-            if(mp.find(s[j])!=mp.end()){
-              mp[s[j]]--;
-                
-            
-            if(mp[s[j]]==0){
-                count--;
-            }
-            }
-            //starting window starts
-            if(j-i+1<k){
+	        if(mp[txt[j]]==0){
+	            count--;
+	        }
+	        }
+            //creating window
+            if(j-i+1 < k){
                 j++;
             }
-            else if(j-i+1==k){
-                //1. ans 2. moving the window
-                if(count==0){
-                    ans.push_back(i);
-                    
-                }
-                //processing for leaving element
-                if(mp.find(s[i])!=mp.end()){
-                    mp[s[i]]++;
-                    if(mp[s[i]]==1){
-                    count++;}
-                }
-            
-            i++;j++;
-        }
-        }
-        return ans.size();
+	        //size check 
+	        else if(j-i+1 == k){
+	            if(count == 0){
+	                ans++;
+	            }
+	            if(mp.find(txt[i])!=mp.end()){
+	                    mp[txt[i]]++;
+	                     if(mp[txt[i]]==1){
+
+	                    count++;
+	                     }
+	                }
+	                i++;j++;
+	            
+	        }
+	        
+	    }
+	    return ans;
 
 	}
 
