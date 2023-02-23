2342665ef4353fde135c216cef6b2a95a4153b90
iamvibhavtripathi <79766183+iamvibhavtripathi@users.noreply.github.com>
2023-02-24 00:59:13 +0530

    Added solution - LeetHub

---
+++ Bit Manipulation - GFG/bit-manipulation.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        // your code here
+        int k=i-1;
+        cout<<(1 & (num>>k))<<" ";
+        
+        cout<<(num | (1<<k))<<" ";
+        
+        cout<< (num & (~(1 << k)));
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n >> i;
+        Solution ob;
+        ob.bitManipulation(n, i);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
